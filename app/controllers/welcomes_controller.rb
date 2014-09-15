aa365b0eb32c2b561b95f789517ecef2efbb3404
Joven Osumo <jovenosumo@yahoo.com>
2014-09-15 04:58:42 +0000

    first commit

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,22 @@
+class WelcomesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def features
+  end
+
+  def faqs
+  end
+
+  def pricing
+  end
+
+  def login
+  end
+end
